24fa11c1dc1c4daaea5838874a92114ec79cc637
Julian Gabriel Niveyro <115017654+NiveJulian@users.noreply.github.com>
2024-08-22 01:24:24 -0300

    Update rutaBack.js

--- client/src/redux/actions/rutaBack.js
+++ client/src/redux/actions/rutaBack.js
@@ -1,4 +1,4 @@
 
-// const rutaBack = "https://server-ninashowroom.vercel.app";
-const rutaBack = 'http://localhost:3001';
+const rutaBack = "https://server-ninashowroom.vercel.app";
+// const rutaBack = 'http://localhost:3001';
 export default rutaBack;
