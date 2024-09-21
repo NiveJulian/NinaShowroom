7f7251128c0956010a4b6fc3cf15a1b752969af4
niveyrojulian <niveyrojulian5@gmail.com>
2024-09-21 19:13:13 -0300

    Estilos de imagenes de cartas

--- client/src/componentes/Ecommerce/Images/ImageComponent.jsx
+++ client/src/componentes/Ecommerce/Images/ImageComponent.jsx
@@ -5,17 +5,11 @@ const ImageComponent = ({ imageUrls }) => {
   const images = imageUrls.split(","); // Asumiendo que las URLs vienen en formato de cadena separada por comas
 
   return (
-    <>
-      {images.map((url, index) => (
-        <div key={index}>
-          <LazyLoadImage
-            src={url.trim()}
-            alt={`Producto ${index + 1}`}
-            className="w-64 h-64 object-cover object-center"
-          />
-        </div>
-      ))}
-    </>
+    <LazyLoadImage
+      src={images[0]}
+      alt={`Producto image`}
+      className="w-64 h-64 object-cover"
+    />
   );
 };
 
